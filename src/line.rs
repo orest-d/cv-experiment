8cde48a67a2973fa63aee6d6f63d55b951c31d0b
Orest Dubay <orest3.dubay@gmail.com>
2019-09-15 00:08:37 +0200

    LinearFit object

--- src/line.rs
+++ src/line.rs
@@ -17,6 +17,87 @@ pub struct WeightedPoint{
     weight: f32,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct LinearFit{
+    x_sum:f32,
+    y_sum:f32,
+    xx_sum:f32,
+    yy_sum:f32,
+    xy_sum:f32,
+    w_sum:f32,
+    xmin:Option<f32>,
+    xmax:Option<f32>,
+    ymin:Option<f32>,
+    ymax:Option<f32>,
+    count:usize
+}
+
+impl LinearFit{
+    pub fn new()->LinearFit{
+        LinearFit{
+            x_sum:0.0,
+            y_sum:0.0,
+            xx_sum:0.0,
+            yy_sum:0.0,
+            xy_sum:0.0,
+            w_sum:0.0,
+            xmin:None,
+            xmax:None,
+            ymin:None,
+            ymax:None,
+            count:0
+        }
+    }
+
+    pub fn add(&mut self,point:WeightedPoint){
+        let WeightedPoint{x:x,y:y,weight:w}=point;
+        self.x_sum += w*x;
+        self.y_sum += w*y;
+        self.xx_sum += w*x*x;
+        self.yy_sum += w*y*y;
+        self.xy_sum += w*x*y;
+        self.w_sum += w;
+        self.count+=1;
+        if w>0.0{
+            self.xmin = Some(self.xmin.map_or(x,|old| old.min(x)));
+            self.xmax = Some(self.xmax.map_or(x,|old| old.max(x)));
+            self.ymin = Some(self.ymin.map_or(y,|old| old.min(y)));
+            self.ymax = Some(self.ymax.map_or(y,|old| old.max(y)));
+        }
+    }
+
+    pub fn line(&self)->Line{
+        let mut line = Line::new();
+        if self.w_sum>0.0{
+            let x = self.x_sum/self.w_sum;
+            let y = self.y_sum/self.w_sum;
+            let xx = self.xx_sum/self.w_sum - x*x;
+            let yy = self.yy_sum/self.w_sum - y*y;
+            let xy = self.xy_sum/self.w_sum - x*y;
+            if xx.abs()>0.0 || yy.abs()>0.0{                
+                line.point.weight=self.w_sum/self.count as f32;
+                if xx.abs()>=yy.abs(){
+                    line.line_type = LineType::FX;
+                    line.point.x = x;
+                    line.point.y = y;
+                    line.k = xy/xx;
+                    line.x1 = self.xmin.unwrap_or(x);
+                    line.x2 = self.xmax.unwrap_or(x);
+                }
+                else{
+                    line.line_type = LineType::FY;
+                    line.point.x = y;
+                    line.point.y = x;
+                    line.k = xy/yy;
+                    line.x1 = self.ymin.unwrap_or(y);
+                    line.x2 = self.ymax.unwrap_or(y);
+                }
+            }
+        }
+        line
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Line{
     line_type: LineType,
@@ -88,7 +169,7 @@ impl Line{
         }
         line
     }
-    
+
     pub fn fit_region(grid:&CharacteristicsGrid, x:usize, y:usize, dx:usize, dy:usize, angle:u8, delta:u8)->Line{
         let mut buffer = [WeightedPoint{x:0.0,y:0.0,weight:0.0};POINT_BUFFER_SIZE];
         let mut count = 0;
@@ -125,7 +206,7 @@ mod tests {
     }
 
     #[test]
-    fn test_fit_simple() {
+    fn test_line_fit_simple() {
         let line = Line::fit(&[ 
                 WeightedPoint{x:0.0,y:0.0,weight:1.0},
                 WeightedPoint{x:1.0,y:1.0,weight:1.0},
@@ -138,6 +219,23 @@ mod tests {
         assert_eq!(line.x1, 0.0);
         assert_eq!(line.x2, 1.0);
     }
+ 
+    #[test]
+    fn test_fit_simple() {
+        let mut fit = LinearFit::new();
+        fit.add(WeightedPoint{x:0.0,y:0.0,weight:1.0});
+        fit.add(WeightedPoint{x:1.0,y:1.0,weight:1.0});
+
+        let line = fit.line();
+        assert_eq!(line.line_type, LineType::FX);
+        assert_eq!(line.point.x, 0.5);
+        assert_eq!(line.point.y, 0.5);
+        assert_eq!(line.point.weight, 1.0);
+        assert_eq!(line.k, 1.0);
+        assert_eq!(line.x1, 0.0);
+        assert_eq!(line.x2, 1.0);
+    }
+ 
     #[test]
     fn test_fit_fy() {
         let line = Line::fit(&[ 
